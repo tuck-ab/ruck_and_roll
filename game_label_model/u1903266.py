25f6f14ce8e1c865d1c0fec0f1357e03b6c9eaca
Xander Tuck <Xander.Tuck@warwick.ac.uk>
2023-02-23 16:18:15 +0000

    Code used to get basic analysis on result of yolo on videos

--- game_label_model/u1903266.py
+++ game_label_model/u1903266.py
@@ -4,6 +4,10 @@ from .bb_cnn_model import BoundingBoxCNN
 from .dir_paths import MODULE_DIR
 
 def run(video_dir, yolo_model_dir):
+    print("Running u1903266 `run`")
+
+
+def get_bb_counts(video_dir, yolo_model_dir):
     vids = [
         "cambridge-v-cinderford-bip.mp4",
         "221015ramsvcambridge-bip.mp4",
@@ -11,28 +15,15 @@ def run(video_dir, yolo_model_dir):
         "2022101cambridgevsale-fc-bip.mp4",
         "20220910bishops-stortfordvcambridgenational-1-bi.mp4",
         "20220924dmpvcambridge.mp4",
-        "20220903cambridgevplymouthnational-1-bip.mp4"
+        "20220903cambridgevplymouthnational-1-bip.mp4",
+        "220611galleivnor_2_movie-001.mov"
     ]
     
-    # for vid in vids:
-    #     bb_cnn = BoundingBoxCNN(
-    #         os.path.join(video_dir, vid),
-    #         ## os.path.join(video_dir, "nootnoot.mp4"),
-    #         os.path.join(yolo_model_dir, "yolov7-tiny_256x320.onnx")
-    #     )
-        
-        
+    for vid in vids:
+        bb_cnn = BoundingBoxCNN(
+            os.path.join(video_dir, vid),
+            os.path.join(yolo_model_dir, "yolov7-tiny_256x320.onnx")
+        )
 
-        # bb_cnn.get_num_bbs_per_frame(save_to_file=os.path.join(MODULE_DIR, "bb_cnn_model", "bb_nums", f"{vid.split('.')[0]}.txt"))
-    # vid = "220611galleivnor_2_movie-001.mov"
-
-    # bb_cnn = BoundingBoxCNN(
-    #     os.path.join(video_dir, vid),
-    #     os.path.join(yolo_model_dir, "yolov7-tiny_256x320.onnx")
-    # )
-
-    # to_save = os.path.join(MODULE_DIR, "bb_cnn_model", "bb_nums", f"{vid.split('.')[0]}.txt")
-
-    # bb_cnn.get_num_bbs_per_frame(save_to_file=to_save)
-
-    print("IT WORKSS LOL")
+        to_save = os.path.join(MODULE_DIR, "bb_cnn_model", "bb_nums", f"{vid.split('.')[0]}.txt")
+        bb_cnn.get_num_bbs_per_frame(save_to_file=to_save)
