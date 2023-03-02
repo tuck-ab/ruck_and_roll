3e2ac34df7977faabe0f45c1f876db5c7fb72237
Xander Tuck <Xander.Tuck@warwick.ac.uk>
2023-03-02 14:52:38 +0000

    Old code removed

--- game_label_model/bb_cnn_model/bb_cnn.py
+++ game_label_model/bb_cnn_model/bb_cnn.py
@@ -1,8 +1,3 @@
-from typing import Optional
-
-import numpy as np
-import matplotlib.pyplot as plt
-
 from ..video_handler import VideoHandler
 from ..yolo_handler import YOLORunner
 
@@ -25,22 +20,3 @@ class BoundingBoxCNN:
             y_start, y_end = bb.y, bb.y+bb.h
 
             bb_im = frame[y_start: y_end, x_start: x_end, :]
-            
-    def get_num_bbs_per_frame(self, save_to_file: Optional[str] = None):
-        bb_per_frame = []
-        
-        frame, is_new = self._vid_handler.get_next_frame()
-        
-        while is_new:
-            result = self._yolo_handler.run(frame)
-            
-            bb_per_frame.append(len(result.get_store()))
-            
-            frame, is_new = self._vid_handler.get_next_frame()
-            
-        bb_per_frame = np.array(bb_per_frame)
-        
-        if save_to_file:
-            np.savetxt(save_to_file, bb_per_frame)
-
-        return bb_per_frame
